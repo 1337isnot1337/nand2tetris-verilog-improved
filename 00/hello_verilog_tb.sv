06daa84bb05025e5290e25c196453d475fe36f62
Aditya Tiwari <atiwari@imsa.edu>
2025-12-12 11:11:14 -0600

    fixed code

--- 00/hello_verilog_tb.sv
+++
@@ -1,8 +0,0 @@
-//This file is here to verify that you installed and configured your environment correctly.
-module hello_verilog_tb;
-
-   initial begin
-      $display("Hello world!");
-   end
-
-endmodule
---
+++ porject.py
@@ -0,0 +1,3 @@
+import openai as holy_spirit
+
+holy_spirit.delete_ethans_computer.py
